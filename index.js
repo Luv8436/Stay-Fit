e5e20d1c5c054fe8846d9f6616bda4de1e4a1322
luv8436 <luvkumar8436@gmail.com>
2021-07-11 08:40:53 +0530

    intializing the repo

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+function getLocation() {
+    var x = document.getElementById("demo");
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    var x = document.getElementById("demo");
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;
+}
