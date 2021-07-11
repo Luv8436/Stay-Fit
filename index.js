7eacebc240a15d9fb076a06761e80a69a3acca2f
luv8436 <luvkumar8436@gmail.com>
2021-07-11 10:54:07 +0530

    display onchanging position

--- index.js
+++ index.js
@@ -10,7 +10,40 @@ function getLocation() {
 }
 
 function showPosition(position) {
-    var x = document.getElementById("demo");
+    var x = document.getElementById("running-status");
     x.innerHTML = "Latitude: " + position.coords.latitude + 
     "<br>Longitude: " + position.coords.longitude;
 }
+
+function getCoordinates(){
+    let togglebtn = document.getElementById('running-btn');
+    if(togglebtn.checked){
+        getLocation();
+    }else{
+        hideLocation();
+    }
+}
+
+function hideLocation(){
+    var x = document.getElementById("running-status");
+    x.innerHTML = "";
+}
+
+const observer = new MutationObserver(mutation => {
+    console.log('DOM mutation detected');
+});
+
+
+  
+window.addEventListener('DOMContentLoaded', (event) => {
+    observer.observe(document.querySelector('#running-status'), {
+        childList: true,
+        attributes: true,
+        subtree: true,
+        characterData: true,
+    });
+});
+                                 
+                             
+             
+            
